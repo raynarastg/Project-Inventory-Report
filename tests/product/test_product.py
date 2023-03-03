81ca73e6513aa5d00649fc7672db02b2f4fb2729
Raynara Santiago <raysantiagobz@gmail.com>
2023-03-03 15:43:20 -0300

    req 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,13 +1,15 @@
 from inventory_report.inventory.product import Product
 
+
 def test_cria_produto():
-    values = Product(8,
-                   'Aspirin',
-                   'Galena Biopha',
-                   '2021-02-22',
-                   '2024-03-14',
-                   'KZ63 800H NM4B ZOWB YYUI',
-                   'instrucao 8')
+    values = Product(
+        8,
+        'Aspirin',
+        'Galena Biopha',
+        '2021-02-22',
+        '2024-03-14',
+        'KZ63 800H NM4B ZOWB YYUI',
+        'instrucao 8')
 
     assert values.id == 8
     assert values.nome_do_produto == 'Aspirin'
