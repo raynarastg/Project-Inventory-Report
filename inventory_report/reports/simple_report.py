35d522866d982124e6b8f39a40d974f9378db58b
Raynara Santiago <raysantiagobz@gmail.com>
2023-03-03 18:34:46 -0300

    req 02

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,17 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(inventory):
+        old_data = min([date['data_de_fabricacao'] for date in inventory])
+        expiration_date = min([date["data_de_validade"] for date in inventory
+                              if (date["data_de_validade"] > datetime.now()
+                               .strftime('%Y-%m-%d'))])
+        empresa = Counter(name['nome_da_empresa'] for name in inventory)
+        return (
+            f"Data de fabricação mais antiga: {old_data}\n"
+            f"Data de validade mais próxima: {expiration_date}\n"
+            f"Empresa com mais produtos: {empresa.most_common()[0][0]}"
+        )
