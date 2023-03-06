8493b3e67946b633a72e6fa6d7ad534133e1c012
Raynara Santiago <raysantiagobz@gmail.com>
2023-03-06 12:05:18 -0300

    req 05

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,5 @@
 import csv
+import json
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -6,7 +7,14 @@ from inventory_report.reports.complete_report import CompleteReport
 class Inventory:
     @staticmethod
     def import_data(path, type):
-        with open(path) as pathCsv:
+        file = path.split('.')[-1]
+        with open(path) as pathReceived:
+            if file == 'csv':
+                value = list(csv.DictReader(pathReceived))
+            elif file == 'json':
+                value = json.load(pathReceived)
             if type == "simples":
-                return SimpleReport.generate(list(csv.DictReader(pathCsv)))
-            return CompleteReport.generate(list(csv.DictReader(pathCsv)))
+                return SimpleReport.generate(value)
+            return CompleteReport.generate(value)
+# ref :
+#  https://stackoverflow.com/questions/541390/extracting-extension-from-filename-in-python
