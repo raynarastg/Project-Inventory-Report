37bfecb731febc075c790bdfb4dd77af57d5e9dd
Raynara Santiago <raysantiagobz@gmail.com>
2023-03-06 14:48:56 -0300

    req 06

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import xml.etree.ElementTree as ET
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -13,8 +14,12 @@ class Inventory:
                 value = list(csv.DictReader(pathReceived))
             elif file == 'json':
                 value = json.load(pathReceived)
+            elif file == 'xml':
+                valuesXml = ET.parse(pathReceived).getroot()
+                value = [{x.tag: x.text for x in line} for line in valuesXml]
             if type == "simples":
                 return SimpleReport.generate(value)
             return CompleteReport.generate(value)
 # ref :
 #  https://stackoverflow.com/questions/541390/extracting-extension-from-filename-in-python
+# https://pt.stackoverflow.com/questions/3561/como-criar-e-ler-um-xml-com-python
